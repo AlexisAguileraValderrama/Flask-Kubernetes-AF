c538c41f08603febdf7205db76df814a898d5dd9
AguileraValderramaAlexis <alexisav2000@hotmail.com>
2023-06-12 21:27:02 -0600

    Pagina web

--- flaskapi.py
+++ flaskapi.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 mysql = MySQL()
 
 # MySQL configurations
-app.config["MYSQL_DATABASE_USER"] = "root"
-app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("db_root_password")
-app.config["MYSQL_DATABASE_DB"] = os.getenv("db_name")
-app.config["MYSQL_DATABASE_HOST"] = os.getenv("MYSQL_SERVICE_HOST")
-app.config["MYSQL_DATABASE_PORT"] = int(os.getenv("MYSQL_SERVICE_PORT"))
-mysql.init_app(app)
+# app.config["MYSQL_DATABASE_USER"] = "root"
+# app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("db_root_password")
+# app.config["MYSQL_DATABASE_DB"] = os.getenv("db_name")
+# app.config["MYSQL_DATABASE_HOST"] = os.getenv("MYSQL_SERVICE_HOST")
+# app.config["MYSQL_DATABASE_PORT"] = int(os.getenv("MYSQL_SERVICE_PORT"))
+# mysql.init_app(app)
 
 
 @app.route("/")
